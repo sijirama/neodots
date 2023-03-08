a61399da6a78a5a767681b7eb49b349465e10cf0
unknown <gbemilesanmi@gmail.com>
2023-03-08 09:22:08 +0100

    Ts fix

--- after/plugin/snippets.lua
+++ after/plugin/snippets.lua
@@ -1,2 +1,3 @@
 require("luasnip").filetype_extend("javascript", {"javascriptreact"})
+require("luasnip").filetype_extend("typescript", {"typescriptreact"})
 require("luasnip").filetype_extend("javascript", {"next"})

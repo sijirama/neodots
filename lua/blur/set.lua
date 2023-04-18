72df337828a16a46dcc2f7065ee514d32d5e0c4c
sijibomi <gbemilesanmi@gmail.com>
2023-04-18 10:08:28 +0100

    autoindent = true

--- lua/blur/set.lua
+++ lua/blur/set.lua
@@ -2,19 +2,13 @@ local set = vim.opt
 
 set.number = true
 set.relativenumber = true
-
+set.autoindent = true
 set.tabstop = 4
 set.shiftwidth = 4
 set.expandtab = true
-
 set.smartindent = true
-
 set.wrap = true
-
-set.hlsearch = false 
+set.hlsearch = false
 set.incsearch = true
-
 set.scrolloff = 7
 set.cursorline = true
-
-

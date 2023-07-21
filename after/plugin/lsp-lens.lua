868b862557ac225a75cbbc82d4f1fcb93c631bd4
sijibomi <gbemilesanmi@gmail.com>
2023-07-21 11:58:10 +0100

    cols

--- after/plugin/lsp-lens.lua
+++ after/plugin/lsp-lens.lua
@@ -2,7 +2,7 @@ require 'lsp-lens'.setup({
     enable = true,
     include_declaration = false, -- Reference include declaration
     sections = {
-                               -- Enable / Disable specific request
+        -- Enable / Disable specific request
         definition = false,
         references = true,
         implementation = true,

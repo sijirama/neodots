5b06214dba5bb18cb04c7cdc0ab7f46ffca8fe4d
sijibomi <gbemilesanmi@gmail.com>
2023-10-26 22:47:50 +0100

    fix: updated indent-blankline config to latest version

--- after/plugin/indent.lua
+++ after/plugin/indent.lua
@@ -1,5 +1,12 @@
-require("indent_blankline").setup {
-    -- for example, context is off by default, use this to turn it on
-    show_current_context = true,
-    show_current_context_start = false,
+local highlight = {
+    "CursorColumn",
+    "Whitespace",
+}
+require("ibl").setup {
+    indent = { highlight = highlight, char = "" },
+    whitespace = {
+        highlight = highlight,
+        remove_blankline_trail = false,
+    },
+    scope = { enabled = false },
 }

9f9b9f001b9c821da2b91fa6de72c0d3ec022efa
sijirama <gbemilesanmi@gmail.com>
2024-02-07 17:10:26 +0100

    migrated to lsp-zero v3.x

--- after/plugin/hover.lua
+++ after/plugin/hover.lua
@@ -5,7 +5,7 @@ require("hover").setup {
         require('hover.providers.gh')
         require('hover.providers.gh_user')
         -- require('hover.providers.jira')
-        -- require('hover.providers.man')
+        require('hover.providers.man')
         require('hover.providers.dictionary')
     end,
     preview_opts = {
--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,162 +1,33 @@
-local lsp = require('lsp-zero')
-local cmp = require 'cmp'
-local luasnip = require "luasnip"
-local lspconfig = require('lspconfig')
+-- WARN: because i'm dumb as fuck, all this config settings are probably scattered and not well organized, siji is dumb as fuck, all of them were gotten from the lsp-zero
 
-lsp.preset('recommended')
+require('lsp-zero')
 
--- lsp.ensure_installed({
---     'tsserver',
--- })
+-- NOTE: set some lsps up
+require('lspconfig').lua_ls.setup({})
+require('lspconfig').gopls.setup({})
+require('lspconfig').tsserver.setup({})
 
 
+-- NOTE: cmp is dumb as fuck
+local cmp = require('cmp')
 
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
--- local cmp_mappings = lsp.defaults.cmp_mappings({
---     --['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
---     --['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
---     ['<C-k>'] = cmp.mapping.select_next_item(cmp_select),
---     ['<C-j>'] = cmp.mapping.select_prev_item(cmp_select),
---     --["<tab>"] = cmp.mapping.select_next_item(),
---     ['<cr>'] = cmp.mapping.confirm({ select = true }),
---     --['<C-Right>'] = cmp.mapping.confirm({ select = true }),
---     ["<C-Space>"] = cmp.mapping.complete(),
--- })
-
---NOTE: the mappings dont work since i converted to linux
+-- NOTE: add lua snippets bro
+require('luasnip.loaders.from_vscode').lazy_load()
 
 cmp.setup({
-    mapping = {
-        ['<C-k>'] = cmp.mapping.select_next_item(),
-        ['<C-j>'] = cmp.mapping.select_prev_item(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        ['<C-Space>'] = cmp.mapping.complete(),
+
+    completion = {
+        --autocomplete = false  NOTE: the docs said that the autocomplete has tp be false, but its the even better when it is false to, to trigger the menu bar
     },
-})
 
---NOTE: end of new code to text
-
--- If you want insert `(` after select function or method item
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-cmp.event:on(
-    'confirm_done',
-    cmp_autopairs.on_confirm_done()
-)
-
-require 'cmp'.setup {
     sources = {
-        { name = 'nvim_lsp' }
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
     },
-    snippet = {
-        expand = function(args)
-            luasnip.lsp_expand(args.body)
-        end,
-    }
-}
 
--- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-require('lspconfig')['lua_ls'].setup {
-    capabilities = capabilities
-}
-require('lspconfig')['pyright'].setup {
-    capabilities = capabilities
-}
-require('lspconfig')['tsserver'].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-    detached = false,
-    flags = lsp_flags,
-    filetypes = { 'typescript', 'typescriptreact' }
-}
-require('lspconfig')['html'].setup {
-    capabilities = capabilities
-}
-require('lspconfig')['cssls'].setup {
-    capabilities = capabilities
-}
-
-require("cmp_nvim_lsp").setup("cssls", {
-    settings = {
-        css = {
-            validate = true,
-            lint = {
-                unknownAtRules = "ignore"
-            }
-        },
-        scss = {
-            validate = true,
-            lint = {
-                unknownAtRules = "ignore"
-            }
-        },
-        less = {
-            validate = true,
-            lint = {
-                unknownAtRules = "ignore"
-            }
-        },
-    },
+    mapping = cmp.mapping.preset.insert({
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
+        ['<C-Space>'] = cmp.mapping.complete(),
+    })
 })
 
-
--- lsp.setup_nvim_cmp({
---     mapping = cmp_mappings
--- })
-
-lsp.on_attach(function(client, bufnr)
-    local opts = { buffer = bufnr, remap = false }
-    if client.name == "eslint" then
-        vim.cmd.LspStop('eslint')
-        return
-    end
-
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-    vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
-    vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-    vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-    vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-    vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-    vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
-    vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
-end)
-
-lsp.setup()
-
-vim.diagnostic.config({
-    virtual_text = true,
-    signs = true,
-    update_in_insert = true,
-    underline = true,
-    severity_sort = true,
-    float = true,
-})
-
---NOTE: my attempt in attaching templ files to the lsp
-
-vim.filetype.add({ extension = { templ = "templ" } })
-
-lspconfig.html.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    filetypes = { "html", "templ" },
-})
-
-lspconfig.htmx.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    filetypes = { "html", "templ" },
-})
-
-lspconfig.tailwindcss.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    filetypes = { "templ", "astro", "javascript", "typescript", "react", "typescriptreact", "javascriptreact" },
-    init_options = { userLanguages = { templ = "html" } },
-})
-
-vim.api.nvim_create_autocmd({ "BufWritePre" }, { pattern = { "*.templ" }, callback = vim.lsp.buf.format })
-
------------------------------------------ attempt ended

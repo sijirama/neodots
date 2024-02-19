e749e4654464f87031926c2adcfbd9bc63e9c57f
sijirama <gbemilesanmi@gmail.com>
2024-02-19 21:52:33 +0100

    nvim tree collapse all key

--- lua/blur/remap.lua
+++ lua/blur/remap.lua
@@ -22,6 +22,7 @@ map('n', '<leader>qq', ':q!<CR>', opts)
 --nvim tree stuff
 map('n', '<leader>e', ':NvimTreeToggle<CR>', opts)
 map('n', '<leader>fe', ':NvimTreeFocus<CR>', opts)
+map('n', '<leader>ef', ':NvimTreeCollapse<CR>', opts)
 
 --zen mode
 map("n", "<leader>zz", ":ZenMode<CR>", opts)

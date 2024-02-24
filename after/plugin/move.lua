99ca9877a07d6a4f45a31e21eec058b59b8e9f51
sijirama <gbemilesanmi@gmail.com>
2024-02-24 02:15:25 +0100

    move added

--- after/plugin/move.lua
+++ after/plugin/move.lua
@@ -1,10 +1,16 @@
+require('move').setup({})
+
+
 local opts = { noremap = true, silent = true }
 
 --PERF: Normal-mode commands
 vim.keymap.set('n', '<A-j>', ':MoveLine(1)<CR>', opts)
 vim.keymap.set('n', '<A-k>', ':MoveLine(-1)<CR>', opts)
-vim.keymap.set('n', '<A-h>', ':MoveHChar(-1)<CR>', opts)
 vim.keymap.set('n', '<A-l>', ':MoveHChar(1)<CR>', opts)
+vim.keymap.set('n', '<A-h>', ':MoveHChar(-1)<CR>', opts)
+
+
+
 
 --PERF: Visual-mode command
 vim.keymap.set('v', '<A-j>', ':MoveBlock(1)<CR>', opts)
